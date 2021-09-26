f7c23412cdf3e4b7475dee363e8553b003f1295b
franklin <franklinwanami@gmail.com>
2021-09-26 08:48:52 +0300

    modified models

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    occupants_count = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
