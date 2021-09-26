8711588d7a42e4b4008e3c7151153f04d18dfb1b
franklin <franklinwanami@gmail.com>
2021-09-26 07:57:34 +0300

    index

--- hood/settings.py
+++ hood/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'neighborhood.apps.NeighborhoodConfig',
-    'bootstrap4'
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
