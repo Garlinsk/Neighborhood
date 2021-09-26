4a5c6f9c87cffe0553d23c482eb0adf0a1dfe0bf
franklin <franklinwanami@gmail.com>
2021-09-26 08:43:37 +0300

    created db

--- hood/settings.py
+++ hood/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'jirani',
+        'USER': 'garlinsk',
+        'PASSWORD': 'kuloba',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
