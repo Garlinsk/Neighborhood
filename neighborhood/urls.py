05b017014869f9a5be484afa94ae3ade17f034a6
franklin <franklinwanami@gmail.com>
2021-09-26 08:37:51 +0300

    connecting to postgres

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
+    url(r'^$',views.index,name = 'index'),
 ]
 
 if settings.DEBUG:
--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime as dt
 
