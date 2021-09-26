659c7a2971ce436248a6867304bacd30920becdb
franklin <franklinwanami@gmail.com>
2021-09-26 08:18:48 +0300

    config base.html

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index,name = 'index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime as dt
 
 # Create your views here.
 
 
-def welcome(request):
-    return HttpResponse('Welcome to Neighborhood')
+def index(request):
+    date = dt.date.today()
+    # all_neighborhoods = Neighborhood.get_neighborhoods()
+    return HttpResponse(request,'index.html')
