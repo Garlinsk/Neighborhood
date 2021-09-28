f5f552353f34b79d3809a36199eb0a04d53632f4
franklin <franklinwanami@gmail.com>
2021-09-28 13:52:41 +0300

    deployment

--- neighborhood/apps.py
+++ neighborhood/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class NeighborhoodConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'neighborhood'
+
+    def ready(self):
+        import neighborhood.signals
--- neighborhood/models.py
+++ neighborhood/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 from tinymce.models import HTMLField
-
+import datetime as dt
 
 # Create your models here.
 
